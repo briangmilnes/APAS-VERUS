74cca597f29a3ba8f139aa6e8d24f03f25a25a98
Brian G. Milnes <briangmilnes@gmail.com>
2026-04-10 05:05:13 -0700

    fix agent3 remaining: start at Chap57 (agent7 does 56)

--- plans/r170-minimize-agent3-remaining.sh
+++ plans/r170-minimize-agent3-remaining.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# R170 Agent3 remaining: Chap56,57,58
+# R170 Agent3 remaining: Chap57,58
 cd ~/projects/APAS-VERUS-agent3
 git status --porcelain | wc -l  # must be 0
 
@@ -7,4 +7,4 @@ git status --porcelain | wc -l  # must be 0
     -c /home/milnes/projects/APAS-VERUS-agent3 \
     -l /home/milnes/projects/APAS-VERUS-agent3/src/vstdplus \
     --project APAS --no-lib-min -a -p --danger --fresh \
-    --chapter Chap56 --chapter Chap57 --chapter Chap58
+    --chapter Chap57 --chapter Chap58
